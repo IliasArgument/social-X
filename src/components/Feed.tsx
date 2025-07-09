ebcae0ff1a4dc0002f0c122d43de5a3200ce1af0
Illia <ilakoles96@gmail.com>
2025-07-09 19:21:20 +0200

    first commit

---
+++ src/components/Feed.tsx
@@ -0,0 +1,68 @@
+import { prisma } from "@/prisma";
+import Post from "./Post";
+import { auth } from "@clerk/nextjs/server";
+import InfiniteFeed from "./InfiniteFeed";
+
+const Feed = async ({ userProfileId }: { userProfileId?: string }) => {
+  const { userId } = await auth();
+
+  if (!userId) return;
+  // Формируем условие для выборки постов:
+  // Если передан userProfileId — это профиль конкретного пользователя,
+  // тогда берём только его посты (корневые, т.е. не комментарии)
+  const whereCondition = userProfileId
+    ? {
+        parentPostId: null, // Только корневые посты (не комментарии)
+        userId: userProfileId, // Только от указанного пользователя
+      } // профиль — только посты этого пользователя
+    : {
+        // Иначе (если профиль не указан) — это главная лента:
+        // Показываем посты текущего пользователя + тех, на кого он подписан
+        parentPostId: null, //только оригинальные посты
+        userId: {
+          in: [
+            userId,
+            ...(
+              await prisma.follow.findMany({
+                where: { followerId: userId }, // Найти всех, на кого он подписан
+                select: { followingId: true }, // Вытащить их ID
+              })
+            ).map((follow) => follow.followingId),
+          ],
+        },
+      };
+
+  const postIncludeQuery = {
+    user: { select: { displayName: true, username: true, img: true } },
+    _count: { select: { likes: true, rePosts: true, comments: true } },
+    likes: { where: { userId: userId }, select: { id: true } },
+    rePosts: { where: { userId: userId }, select: { id: true } },
+    saves: { where: { userId: userId }, select: { id: true } },
+  };
+
+  const posts = await prisma.post.findMany({
+    where: whereCondition,
+    include: {
+      rePost: {
+        include: postIncludeQuery,
+      },
+      ...postIncludeQuery,
+    },
+    take: 3,
+    skip: 0,
+    orderBy: { createdAt: "desc" },
+  });
+
+  return (
+    <div className="">
+      {posts.map((post) => (
+        <div key={post.id}>
+          <Post post={post} />
+        </div>
+      ))}
+      <InfiniteFeed userProfileId={userProfileId} />
+    </div>
+  );
+};
+
+export default Feed;
